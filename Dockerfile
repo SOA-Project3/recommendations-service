3c6f7f3cb76743e427fd5c63634800924db2f4d3
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2024-06-11 08:37:06 -0600

    fix: update path to /recommendations

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ RUN npm install
  
 COPY . .
  
-EXPOSE 8000
+EXPOSE 3003
 
 
 CMD [ "node", "src/server.js" ]
--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 const express = require("express"); //Import express framework module
 const morgan = require("morgan"); //Import morgan for middleware to log HTTP requests and errors
-const port = 8000; //Define port: first checks if available in environment variables
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const cors = require("cors");
+const port = 3003; //Define port: first checks if available in environment variables
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express(); //Main express app
-const router = express.Router(); 
+const router = express.Router();
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(morgan("tiny")); //Log request
 
@@ -15,14 +16,18 @@ const errorHandler = require("./errors/RecommendationError");
 router.use(errorHandler.queryValidatorMiddleware);
 
 const recommendationRoutes = require("./controllers/CustomRecommendation");
-router.get("/custom", recommendationRoutes.getRecommendation); //Define path for recommendation requests
+
+// Use the cors middleware
+app.use(cors());
+
+router.get("/recommendations", recommendationRoutes.getRecommendation); //Define path for recommendation requests
 
 app.use(router); // Applying the router middleware to the app
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
 module.exports = {
-    app
+  app,
 };
